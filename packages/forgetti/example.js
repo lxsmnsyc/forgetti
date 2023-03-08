a522fde57e6fa970ccf6e7182659e48bab68fe55
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-08 22:25:07 +0800

    Create example.js

---
+++ packages/forgetti/example.js
@@ -0,0 +1,29 @@
+import * as babel from '@babel/core';
+import plugin from 'forgetti';
+
+const options = {
+  preset: 'react',
+};
+
+async function compile(code) {
+  const result = await babel.transformAsync(code, {
+    plugins: [
+      [plugin, options],
+    ],
+    parserOpts: {
+      plugins: [
+        'jsx',
+      ],
+    },
+  });
+
+  return result?.code ?? '';
+}
+
+console.log(await compile(`
+function Example(props) {
+  for (const x in props.arr) {
+    console.log(x);
+  }
+}
+`));
