bbb30790071321ba751984e7827eb577ededbd02
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-20 18:13:59 +0800

    Optimize `isGuaranteedLiteral`

--- packages/forgetti/src/core/is-guaranteed-literal.ts
+++ packages/forgetti/src/core/is-guaranteed-literal.ts
@@ -1,10 +1,16 @@
 import * as t from '@babel/types';
-import { isNestedExpression } from './checks';
 
 export default function isGuaranteedLiteral(node: t.Node): node is t.Literal {
-  if (t.isLiteral(node)) {
-    // Check if it is template literal but with only static expressions
-    if (t.isTemplateLiteral(node)) {
+  switch (node.type) {
+    case 'BigIntLiteral':
+    case 'BooleanLiteral':
+    case 'DecimalLiteral':
+    case 'NullLiteral':
+    case 'NumericLiteral':
+    case 'RegExpLiteral':
+    case 'StringLiteral':
+      return true;
+    case 'TemplateLiteral': {
       let expr: t.Expression | t.TSType;
       for (let i = 0, len = node.expressions.length; i < len; i++) {
         expr = node.expressions[i];
@@ -18,36 +24,40 @@ export default function isGuaranteedLiteral(node: t.Node): node is t.Literal {
       }
       return true;
     }
-    return true;
-  }
-  if (isNestedExpression(node)) {
-    return isGuaranteedLiteral(node.expression);
-  }
-  if (t.isUnaryExpression(node)) {
-    if (node.operator === 'throw' || node.operator === 'delete') {
+    case 'ParenthesizedExpression':
+    case 'TypeCastExpression':
+    case 'TSAsExpression':
+    case 'TSSatisfiesExpression':
+    case 'TSNonNullExpression':
+    case 'TSTypeAssertion':
+    case 'TSInstantiationExpression':
+      return isGuaranteedLiteral(node.expression);
+    case 'UnaryExpression':
+      switch (node.operator) {
+        case 'throw':
+        case 'delete':
+          return false;
+        default:
+          return isGuaranteedLiteral(node.argument);
+      }
+    case 'ConditionalExpression':
+      return isGuaranteedLiteral(node.test)
+        && isGuaranteedLiteral(node.consequent)
+        && isGuaranteedLiteral(node.alternate);
+    case 'BinaryExpression':
+      if (node.operator === '|>') {
+        return false;
+      }
+      if (t.isExpression(node.left)) {
+        return isGuaranteedLiteral(node.left);
+      }
+      if (t.isExpression(node.right)) {
+        return isGuaranteedLiteral(node.right);
+      }
       return false;
-    }
-    return isGuaranteedLiteral(node.argument);
-  }
-  if (t.isConditionalExpression(node)) {
-    return isGuaranteedLiteral(node.test)
-      || isGuaranteedLiteral(node.consequent)
-      || isGuaranteedLiteral(node.alternate);
-  }
-  if (t.isBinaryExpression(node)) {
-    if (node.operator === 'in' || node.operator === 'instanceof' || node.operator === '|>') {
+    case 'LogicalExpression':
+      return isGuaranteedLiteral(node.left) && isGuaranteedLiteral(node.right);
+    default:
       return false;
-    }
-    if (t.isExpression(node.left)) {
-      return isGuaranteedLiteral(node.left);
-    }
-    if (t.isExpression(node.right)) {
-      return isGuaranteedLiteral(node.right);
-    }
-    return false;
   }
-  if (t.isLogicalExpression(node)) {
-    return isGuaranteedLiteral(node.left) || isGuaranteedLiteral(node.right);
-  }
-  return false;
 }
