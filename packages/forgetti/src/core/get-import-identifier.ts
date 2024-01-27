864be6050121d0efb2c3cefdb7614fa92a1954d5
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-27 18:11:37 +0800

    Remove `@babel/helper-module-imports`

--- packages/forgetti/src/core/get-import-identifier.ts
+++ packages/forgetti/src/core/get-import-identifier.ts
@@ -1,24 +1,37 @@
 import type * as babel from '@babel/core';
-import type * as t from '@babel/types';
-import { addDefault, addNamed } from '@babel/helper-module-imports';
+import * as t from '@babel/types';
 import type { ImportDefinition } from './presets';
 import type { StateContext } from './types';
 
-export default function getImportIdentifier(
-  ctx: StateContext,
+export function getImportIdentifier(
+  state: StateContext,
   path: babel.NodePath,
-  definition: ImportDefinition,
+  registration: ImportDefinition,
 ): t.Identifier {
-  const name = definition.kind === 'default' ? 'default' : definition.name;
-  const target = `${definition.source}[${name}]`;
-  const current = ctx.imports.get(target);
+  const name = registration.kind === 'named' ? registration.name : 'default';
+  const target = `${registration.source}[${name}]`;
+  const current = state.imports.get(target);
   if (current) {
     return current;
   }
-  const newID =
-    definition.kind === 'named'
-      ? addNamed(path, definition.name, definition.source)
-      : addDefault(path, definition.source);
-  ctx.imports.set(target, newID);
-  return newID;
+  const programParent = path.scope.getProgramParent();
+  const uid = programParent.generateUidIdentifier(
+    registration.kind === 'named' ? registration.name : 'default',
+  );
+  const newPath = (
+    programParent.path as babel.NodePath<t.Program>
+  ).unshiftContainer(
+    'body',
+    t.importDeclaration(
+      [
+        registration.kind === 'named'
+          ? t.importSpecifier(uid, t.identifier(registration.name))
+          : t.importDefaultSpecifier(uid),
+      ],
+      t.stringLiteral(registration.source),
+    ),
+  )[0];
+  programParent.registerDeclaration(newPath);
+  state.imports.set(target, uid);
+  return uid;
 }
